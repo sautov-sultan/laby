fe7e0920b6bc85193a044d6983e089e83ea869ff
Sultan <sautov.sultan@gmail.com>
2023-02-01 23:47:24 +0600

    byli dobavleny funkcii

--- 2lab.py
+++ 2lab.py
@@ -3,33 +3,42 @@
 
 from time import sleep
 
-chislo = int(input('загадай число от 1 до 100... -> '))
-variants = [i for i in range(1, 101)]
-try_counter = 0
 
-sleep(0.3)
+def game(chislo: int) -> None:
 
-low = 0
-high = 99
+    '''Это текстовая игра "угадайка"'''
 
-while True:
+    variants = [i for i in range(1, 101)]
+    try_counter = 0
+
+    sleep(0.3)
+
+    low = 0
+    high = 99
+
+    while True:
 
 
-    mid = (low + high) // 2
-    guess = variants[mid]
+        mid = (low + high) // 2
+        guess = variants[mid]
 
-    if guess > chislo:
-        high = mid - 1
-    else:
-        low = mid + 1
+        if guess > chislo:
+            high = mid - 1
+        else:
+            low = mid + 1
 
-    print(f'ваше число: {guess}?')
-    try_counter += 1
+        print(f'ваше число: {guess}?')
+        try_counter += 1
 
-    ans = input('угадал ли число?: y/n -> ').lower()
-    if ans == 'y':
-        print('супер!')
-        print(f'всего потребовалось {try_counter} попыток.')
-        break
-    else:
-        print('окей...')
+        ans = input('угадал ли число?: y/n -> ').lower()
+        if ans == 'y':
+            print('супер!')
+            print(f'всего потребовалось {try_counter} попыток.')
+            break
+        else:
+            print('окей...')
+
+
+if __name__ == '__main__':
+    digit = int(input('загадай число от 1 до 100... -> '))
+    game(digit)
