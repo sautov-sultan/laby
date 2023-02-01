fe3d8b100ef02c32ad57420b307d9e4886945dff
Sultan <sautov.sultan@gmail.com>
2023-02-01 23:40:58 +0600

    Igra Ugadaika

---
+++ 2lab.py
@@ -0,0 +1,35 @@
+#Саутов Султан 3 курс Computer Science
+#2 лабораторная работа
+
+from time import sleep
+
+chislo = int(input('загадай число от 1 до 100... -> '))
+variants = [i for i in range(1, 101)]
+try_counter = 0
+
+sleep(0.3)
+
+low = 0
+high = 99
+
+while True:
+
+
+    mid = (low + high) // 2
+    guess = variants[mid]
+
+    if guess > chislo:
+        high = mid - 1
+    else:
+        low = mid + 1
+
+    print(f'ваше число: {guess}?')
+    try_counter += 1
+
+    ans = input('угадал ли число?: y/n -> ').lower()
+    if ans == 'y':
+        print('супер!')
+        print(f'всего потребовалось {try_counter} попыток.')
+        break
+    else:
+        print('окей...')
